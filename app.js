b65ae26f0162e7a17e30f7e26842486eb882bb4d
valerijakirjackaja <99329507+valerijakirjackaja@users.noreply.github.com>
2022-02-09 15:13:55 +0000

    Add filter logic

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const url =
 const vizContainer = document.getElementById("vizContainer");
 const vizOptions = {
   device: "desktop",
+  Department: ["Financial Services", "Commercial"], //filtering before dashboard loads
 };
 let viz;
 //1. Grab button from HTML
@@ -33,7 +34,30 @@ function exportPPT() {
   viz.showExportPowerPointDialog();
 }
 
+//function to grab filter values
+function getRangeValues() {
+  //get values from input boxes
+  const minValue = document.getElementById("minValue").value;
+  const maxValue = document.getElementById("maxValue").value;
+  //get the workbook
+  const workbook = viz.getWorkbook();
+  //get active sheet - dashaboard
+  const activeSheet = workbook.getActiveSheet();
+  const sheets = activeSheet.getWorkseets();
+  const sheetToFilter = sheets[1]; //need to link sheets in the dashboard
+  sheetToFilter.applyRangeFilterAsync("SUM(Sales)"),
+    {
+      min: minValue,
+      max: maxValue,
+    };
+}
+
 //wait till content has loaded and execute initViz
 document.addEventListener("DOMContentLoaded", initViz);
 pdfButton.addEventListener("click", exportPDF);
 pptButton.addEventListener("click", exportPPT);
+
+//filter the dashboard for sales
+document
+  .getElementById("filterButton")
+  .addEventListener("click", getRangeValues);
